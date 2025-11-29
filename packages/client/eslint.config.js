88557d644e55f72311ca0062b9e86ff1dc34a417
Alex <38507729+OrmEmbaar@users.noreply.github.com>
2025-11-29 12:59:56 +0000

    chore(eslint): add eslint.config.js files and configure unused vars
    - Add eslint.config.js to all packages (client, policies, server-core, shared)
    - Configure @typescript-eslint/no-unused-vars to allow underscore-prefixed variables
    - Enables standard pattern for intentionally unused parameters

---
+++ packages/client/eslint.config.js
@@ -0,0 +1,3 @@
+import { config } from "@solana-signer/eslint-config/base";
+
+export default config;
---
+++ packages/shared/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@solana-signer/eslint-config/base";
+
+export default config;
+
